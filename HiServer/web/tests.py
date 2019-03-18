b4046579c3ff86fcb4b11e9706759d59d3f75755
梦中琴歌 <38162672+qiangayz@users.noreply.github.com>
2019-03-19 00:29:25 +0800

    123
    123

--- HiServer/web/tests.py
+++ HiServer/web/tests.py
@@ -1,3 +1,10 @@
-from django.test import TestCase
+# from django.test import TestCase
 
 # Create your tests here.
+import pyautogui
+import time
+
+time.sleep(5)
+while True:
+    time.sleep(0.2)
+    pyautogui.rightClick()
